3c0fccc8d45cff18647f8ed69286345a09d7bf81
qlraor09 <qlraor10@gmail.com>
2020-01-22 17:02:35 +0900

    first commit

---
+++ test/test.py
@@ -0,0 +1,19 @@
+book_title = ['great', 'expectations', 'the',
+                 'adventures', 'of', 'sherlock',
+                  'holmes', 'the', 'great',
+                   'gasby', 'hamlet', 'adventures',
+                    'of', 'huckleberry', 'fin'
+            ]
+
+result_dict = {}
+for title in book_title:
+
+        book_title.count
+        # if title in result_dict:
+        #         # result_dict[title] = title.count
+        #         result_dict.count()
+        # else:
+        #         result_dict[title] = 1
+
+
+print(result_dict)
